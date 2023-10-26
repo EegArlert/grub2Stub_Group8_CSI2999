3812dd6535232a2703d35457e9939ef336935238
EegArlert <bsyahputra@oakland.edu>
2023-10-25 21:11:11 -0400

    changeToJdk21

--- grub2Stub/src/main/java/com/example/grub2stub/HelloController.java
+++ grub2Stub/src/main/java/com/example/grub2stub/HelloController.java
@@ -1,9 +1,14 @@
 package com.example.grub2stub;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+
+    @FXML
+    private Button anotherButton;
+
     @FXML
     private Label welcomeText;
 
