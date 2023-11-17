37143e6420055cbb7f8458256d12a63dd2c652da
yshen267 <yshen267@gmail.com>
2023-11-17 17:51:57 -0500

    Adding Nick's updated GUI

--- grub2Stub/src/main/java/com/example/grub2stub/CheckoutController.java
+++ grub2Stub/src/main/java/com/example/grub2stub/CheckoutController.java
@@ -7,13 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
-import javafx.scene.control.TextFormatter;
-import java.util.function.UnaryOperator;
-import java.util.regex.Pattern;
 
 public class CheckoutController {
     @FXML
@@ -25,104 +19,14 @@ public class CheckoutController {
     @FXML
     private Button btnBackHome;
 
-    @FXML
-    private TextField creditCardName;
 
     @FXML
-    private TextField creditCardNumber;
-
-    @FXML
-    private TextField creditCardCVV;
-
-    @FXML
-    private TextField creditCardEXP;
-
-    @FXML
-    private TextField rowNumber;
-
-    @FXML
-    private TextField seatNumber;
-
-    @FXML
-    private CheckBox confirmAge;
-
-    @FXML
-    void initialize() {
-        // Set up TextFormatter for creditCardNumber
-        setupNumericTextField(creditCardNumber);
-
-        // Set up TextFormatter for creditCardCVV
-        setupNumericTextField(creditCardCVV);
-    }
-
-    private void setupNumericTextField(TextField textField) {
-        UnaryOperator<TextFormatter.Change> filter = change -> {
-            String newText = change.getControlNewText();
-            if (isValidNumericInput(newText)) {
-                return change;
-            } else {
-                return null; // Reject the change
-            }
-        };
-
-        TextFormatter<Integer> textFormatter = new TextFormatter<>(filter);
-        textField.setTextFormatter(textFormatter);
-    }
-
-    // Helper method to check if the input is a valid numeric value
-    private boolean isValidNumericInput(String input) {
-        return Pattern.matches("\\d*", input);
-    }
-
-    public boolean luhnTest(String cardNumber){
-        int sum = 0;
-        int numDigits = cardNumber.length();
-        boolean alternator = false;
-
-        //check valid range of total # of digits
-        if (numDigits < 7 || numDigits > 19){
-            return false;
-        }
-
-        for (int i = (numDigits - 1); i >= 0; i--){
-            int d = cardNumber.charAt(i) - '0';
-
-            if (alternator){
-                d *= 2;
-            }
-            sum += (d / 10);
-            sum += (d % 10);
-            alternator = !alternator;
-        }
-        return (sum % 10 == 0);
-    }
-
-    @FXML
-    void btnConfirmClick() {
-        TextField[] allTextFields = {creditCardName, creditCardNumber, creditCardCVV, creditCardEXP, rowNumber, seatNumber};
-
-        boolean anyInputFieldEmpty = false;
-
-        //Check for incomplete user input
-        for (TextField currentTextfield: allTextFields) {
-            if (currentTextfield.getText().isEmpty()) {
-                anyInputFieldEmpty = true;
-                break;
-            }
-        }
-
-        if (anyInputFieldEmpty) {
-            //Placeholder output
-            System.out.println("At least one Textfield is empty.");
-        }
-        else if (!luhnTest(creditCardNumber.getText())){
-            //Placeholder output
-            System.out.println("Credit Card is invalid.");
-        }
-        else {
-            //Create a report to food distributor/order confirmation?
-            System.out.println("Order has been accepted!");
-        }
+    void btnConfirmClick(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("OrderConfirmed.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
