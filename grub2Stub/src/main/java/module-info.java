20d5679780456742bdd4114f557bb4181b508ef6
yshen267 <yshen267@gmail.com>
2023-11-17 16:20:12 -0500

    Merge branch 'Alan'

--- grub2Stub/src/main/java/module-info.java
+++ grub2Stub/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.example.grub2stub {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.xerial.sqlitejdbc;
-
 
     opens com.example.grub2stub to javafx.fxml;
     exports com.example.grub2stub;
